455b23a0d28c00854e732063edbf3a00beb9b227
oscni938 <48986377+oscni938@users.noreply.github.com>
2019-04-02 11:26:02 +0200

    Update site.js

--- site.js
+++ site.js
@@ -65,7 +65,7 @@ function getTimeToFirstPaintIfSupported() {
     if (window.chrome && window.chrome.loadTimes) {
       fpTime = window.performance.getEntriesByType('paint');
       //fpTime = fpTime.startTime;
-      fpTime = fpTime.forEach((fpTime) =>{retunr fpTime.startTime;});
+      fpTime = fpTime.forEach((fpTime) =>{return fpTime.startTime;});
     }
     // If IE/Edge, use the prefixed `msFirstPaint` property.
     // See http://msdn.microsoft.com/ff974719
