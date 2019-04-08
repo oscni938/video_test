4cfa3d0f3ba14f437f415f01e551f16904c03757
oscni938 <48986377+oscni938@users.noreply.github.com>
2019-04-08 11:10:59 +0200

    Update SW_stat.js

--- SW_stat.js
+++ SW_stat.js
@@ -68,10 +68,10 @@ self.addEventListener('fetch', e =>
     fetch(e.request).catch(() => caches.match(e.request))
   );*/
         myRequest = new Request('/book_test/trumpswears.jpg');
-            caches.delete(myRequest).then(function() {
+      /*      caches.delete(myRequest).then(function() {
         console.log("GitRekt");
         
-});
+});*/
       fetch('The_Doll_Bloodborne_ver2.jpg')
         .then(function(response){
         console.log("swapped");
