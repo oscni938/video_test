dadf0c8e7ed12c7e1e41de2cb448e929390365cd
oscni938 <48986377+oscni938@users.noreply.github.com>
2019-04-08 09:46:54 +0200

    Update SW_stat.js

--- SW_stat.js
+++ SW_stat.js
@@ -69,10 +69,10 @@ self.addEventListener('fetch', e =>
   );*/
   e.respondWith(function(){
     // Try the cache
-    /*caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function(response) {
       // Fall back to network
-      return response || fetch(e.request);*/
-    return caches.match('/book_test/trumpswears.jpg');
-    }//)
+      return response || fetch(e.request);
+    //return caches.match('/book_test/trumpswears.jpg');
+    })
   );
 });
