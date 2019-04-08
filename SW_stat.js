784382c099b08abbb5b6420215555fc4691afc73
oscni938 <48986377+oscni938@users.noreply.github.com>
2019-04-08 09:35:05 +0200

    test ret

--- SW_stat.js
+++ SW_stat.js
@@ -69,9 +69,10 @@ self.addEventListener('fetch', e =>
   );*/
   e.respondWith(
     // Try the cache
-    caches.match(e.request).then(function(response) {
+    /*caches.match(e.request).then(function(response) {
       // Fall back to network
-      return response || fetch(e.request);
+      return response || fetch(e.request);*/
+    return caches.match('/book_test/trumpswears.jpg');
     })
   );
 });
